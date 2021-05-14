6e58063dcea1c634ddc3d1651ac0e3ff512d3118
Alex Blake <alex@epione.net>
2021-05-14 22:53:34 +0200

    revert change to original repo default use of funcName on operationId, but allow override in config

--- src/DefinitionGenerator.ts
+++ src/DefinitionGenerator.ts
@@ -132,7 +132,7 @@ export class DefinitionGenerator {
     documentationConfig
   ): Operation {
     const operationObj: Operation = {
-      operationId: documentationConfig.summary.replaceAll(" ", "")
+      operationId: funcName // default to function name
     };
 
     // allow override of operationId property
